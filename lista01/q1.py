ef69c142f385a9f035f6929309f64fa9953fd71c
Givanaldo <givanaldo@gmail.com>
2023-06-19 07:43:53 -0300

    Atualização das questões

---
+++ lista01/q1.py
@@ -0,0 +1,6 @@
+n1 = int(input("Número 1: "))
+n2 = int(input("Número 2: "))
+soma = n1 + n2
+# f"..." --> formatted
+print(f"{n1} + {n2} = {soma}")
+
---
+++ lista01/q2.py
@@ -0,0 +1,3 @@
+m = int(input("Valor em metros: "))
+mm = m * 1000
+print(f"{m} m = {mm} mm")
