d134ce3a75e9edf3e936ae533723938a7c292a54
Givanaldo <givanaldo@gmail.com>
2023-06-19 07:52:35 -0300

    Atualização

---
+++ lista01/q3.py
@@ -0,0 +1,6 @@
+d = int(input("Dias: "))
+h = int(input("Horas: "))
+m = int(input("Minutos: "))
+s = int(input("Segundos: "))
+total = d*24*60*60 + h*60*60 + m*60 + s
+print(f"Total em segundos: {total}")
