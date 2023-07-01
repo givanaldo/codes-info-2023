4b287efff063550f86dfd6ddbf0d187b5b8942f7
Givanaldo <givanaldo@gmail.com>
2023-07-01 18:23:49 -0300

    pentagono

--- turtle/04-pentagono.py
+++ turtle/04-pentagono.py
@@ -1,8 +1,12 @@
 from turtle import *
 
 pensize(3)
+pencolor("blue")
+fillcolor("yellow")
+begin_fill()
 for _ in range(5):
-    forward(100)
+    forward(150)
     right(72)
+end_fill()
 exitonclick()
 
