f5d6726faff7f5ef55077ff9c61a80aad92ba02f
Givanaldo <givanaldo@gmail.com>
2023-07-03 07:26:37 -0300

    lista 1

---
+++ lista01/q6.py
@@ -0,0 +1,6 @@
+# tempo da viagem de um carro
+# v = d / t => t = v / d
+distancia = int(input("Distância: "))
+velocidade = int(input("Velocidade: "))
+tempo = velocidade / distancia
+print(f"Tempo = {tempo}")
---
+++ lista01/q7.py
@@ -0,0 +1,4 @@
+# F = 9*C/5 + 32
+c = int(input("Temperatura em Celsius: "))
+f = 9*c/5 + 32
+print(f"Temperatura em Fahrenheit: {f}")
