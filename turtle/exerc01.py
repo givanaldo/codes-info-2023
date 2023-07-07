694506d619a42516c68d038ccd0cd1a3dfdfab45
Givanaldo <givanaldo@gmail.com>
2023-07-07 08:16:02 -0300

    turtle

--- turtle/exerc01.py
+++ turtle/exerc01.py
@@ -1,24 +1,32 @@
 from turtle import *
-
+fillcolor('red')
+begin_fill()
 penup()
 setposition(-300,0)
 pendown()
 for _ in range(4):
     forward(100)
     left(90)
+end_fill()
 
+fillcolor('blue')
+begin_fill()
 penup()
 setposition(-100,0)
 pendown()
 for _ in range(5):
     forward(120)
     left(72)
+end_fill()
 
+fillcolor('yellow')
+begin_fill()
 penup()
 setposition(200,0)
 pendown()
 for _ in range(8):
     forward(120)
     left(45)
+end_fill()
 
 exitonclick()
---
+++ turtle/exerc02.py
@@ -0,0 +1,8 @@
+from turtle import *
+color('red', 'black')
+begin_fill()
+for _ in range(5):
+    forward(300)
+    left(144)
+end_fill()
+exitonclick()
