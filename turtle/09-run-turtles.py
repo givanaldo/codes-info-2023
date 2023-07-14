112cf4f16c4e113e68d2c8eee82cae241fd72d89
Givanaldo <givanaldo@gmail.com>
2023-07-14 07:45:19 -0300

    turtle

--- turtle/09-run-turtles.py
+++ turtle/09-run-turtles.py
@@ -9,8 +9,10 @@ player_one.color("green")
 player_one.shape("turtle")
 player_one.penup()
 player_one.goto(-200,100)
-player_two = player_one.clone()
+
+player_two = turtle.Turtle()
 player_two.color("blue")
+player_two.shape("turtle")
 player_two.penup()
 player_two.goto(-200,-100)
 
