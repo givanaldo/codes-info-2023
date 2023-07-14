e7084b30ea76342db71bb916993d889397a308b5
Givanaldo <givanaldo@gmail.com>
2023-07-14 08:13:45 -0300

    run-turtle

--- turtle/09-run-turtles.py
+++ turtle/09-run-turtles.py
@@ -4,6 +4,7 @@ import time
 
 vetor = [1,2,3,4,5,6]
 
+# criação das tartarugas
 player_one = turtle.Turtle()
 player_one.color("green")
 player_one.shape("turtle")
@@ -16,6 +17,13 @@ player_two.shape("turtle")
 player_two.penup()
 player_two.goto(-200,-100)
 
+player_three = turtle.Turtle()
+player_three.color("purple")
+player_three.shape("turtle")
+player_three.penup()
+player_three.goto(-200, 0)
+
+# criação da chegada das tartarugas
 player_one.setpos(300,60)
 player_one.pendown()
 player_one.circle(40)
@@ -28,18 +36,30 @@ player_two.circle(40)
 player_two.penup()
 player_two.goto(-200,-100)
 
+player_three.setpos(300,-40)
+player_three.pendown()
+player_three.circle(40)
+player_three.penup()
+player_three.goto(-200, 0)
+
+# início da corrida
 for i in range(20):
-  if player_one.pos() >= (300,100):
-    turtle.write("Player One Wins!", font=("Arial", 15, "normal"))
+  if player_one.pos() >= (300, 100):
+    player_one.write("Player One Wins!", font=("Arial", 20, "normal"))
     break
-  elif player_two.pos() >= (300,-100):
-    turtle.write("Player Two Wins!", font=("Arial", 15, "normal"))
+  elif player_two.pos() >= (300, -100):
+    player_two.write("Player Two Wins!", font=("Arial", 20, "normal"))
+    break
+  elif player_three.pos() >= (300, 0):
+    player_three.write("Player Three Wins!", font=("Arial", 20, "normal"))
     break
   else:
     velocidade = random.choice(vetor)
     player_one.forward(20*velocidade)
     velocidade = random.choice(vetor)
     player_two.forward(20*velocidade)
+    velocidade = random.choice(vetor)
+    player_three.forward(20*velocidade)
     time.sleep(1)
 
 turtle.exitonclick()
