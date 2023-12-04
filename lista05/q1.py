8dbd23a68762678129be1d32c943d9e907d6ef8b
Givanaldo <givanaldo@gmail.com>
2023-12-04 10:25:04 -0300

    lista 5

--- lista05/q1.py
+++ lista05/q1.py
@@ -1,7 +1,8 @@
-import random
+import random as rd
 lista = []
 for _ in range(10):
-    lista.append(random.randint(1, 100))
+    lista.append(rd.randint(1, 100))
 print(lista)
-print(f'Maior: {max(lista)}')
-print(f'Menor: {min(lista)}')
+lista.sort()
+print(f'Maior: {lista[len(lista)-1]}')
+print(f'Menor: {lista[0]}')
---
+++ lista05/q4.py
@@ -0,0 +1,12 @@
+texto = '''The Python Software Foundation and the global Python
+community welcome and encourage participation by everyone. Our community is based on
+mutual respect, tolerance, and encouragement, and we are working to help each other live up
+to these principles. We want our community to be more diverse: whoever you are, and
+whatever your background, we welcome you.
+'''
+lista = texto.split()
+pontuacao = ',.:'
+for p in pontuacao:
+    for i, palavra in enumerate(lista):
+        lista[i] = palavra.replace(p, "")
+print(lista)
